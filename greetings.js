814a3ecfbd1747a7f71eb8d5092518c64e94a065
LJaquez <letty.jaquez@gmail.com>
2022-04-26 13:49:15 -0400

    unit test files

---
+++ greetings.js
@@ -0,0 +1,18 @@
+function Greet(name){
+    if name === null{
+        console.log('Hello there!');
+    };
+    if name === name.upperCase{
+        console.log('HELLO '+ name);
+    };
+    if name === isArray{
+        let nameList = ''
+        for i in name{
+            nameList=nameList + i.stringify+', '
+        };
+        console.log('Hello, '+ nameList);
+    };
+    else{
+        console.log ('Hello, '+ name);
+    };
+    };
